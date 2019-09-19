3e66855cf8eabf31daba187097075de8e4a33fde
yexiaohui <yexiaohui@meituan.com>
2019-09-19 19:15:55 +0800

    feature:完善localStorage

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,89 @@
+import 'dart:convert';
+import "package:universal_html/html.dart" as html;
 import 'package:flutter/material.dart';
 import 'package:toast/toast.dart';
 import 'package:radial_button/widget/circle_floating_button.dart';
 
 void main() => runApp(MyApp());
 
+class PlayerStore {
+  String name = "";
+  int gender = 0;
+  int count = 1;
+  int finalCash = 0;
+  String last = "";
+
+  PlayerStore(String name, int gender, int count, int finalCash, String last) {
+    print("$name $gender $count $last $finalCash");
+    this.name = name;
+    this.gender = gender;
+    this.count = count;
+    this.last = last;
+    this.finalCash = finalCash;
+  }
+
+  factory PlayerStore.fromJson(Map<String, dynamic> parsedJson){
+    return PlayerStore(
+      parsedJson['name'],
+      parsedJson['gender'],
+      parsedJson['count'],
+      parsedJson['finalCash'],
+      parsedJson['last'],
+    );
+  }
+
+  Player playerStore2Player() {
+    print("44444444");
+    return Player(
+      this.name,
+      this.gender,
+      this.count,
+      this.finalCash,
+      this.last,
+    );
+  }
+}
 class Player {
   String name = "";
   int gender = 0;
   int count = 1;
   int finalCash = 0;
   final last = TextEditingController();
-  Player(this.name, this.gender, this.count);
+  Player(String name, int gender, int count, int finalCash, [String last]) {
+    this.name = name;
+    this.gender = gender;
+    this.count = count;
+    this.finalCash = finalCash;
+    this.last.text = last;
+  }
+
+  PlayerStore player2PlayerStore() {
+    return PlayerStore(
+      this.name,
+      this.gender,
+      this.count,
+      this.finalCash,
+      this.last.text,
+    );
+  }
+
+
+}
+
+class SumaryArguments {
+  List<PlayerStore> players;
+  String ratio;
+
+  SumaryArguments(this.players, this.ratio);
+  
+  factory SumaryArguments.fromJson(Map<String, dynamic> parsedJson){
+    var list = parsedJson['players'] as List;
+    List<PlayerStore> players = list.map((i) => PlayerStore.fromJson(i)).toList();
+    return SumaryArguments(
+      players,
+      parsedJson['ratio'],
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -59,13 +132,56 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
-    _ratioController.text = _ratio;
     super.initState();
+    // SumaryArguments settings;
+    if (html.window.localStorage.containsKey('texas_holdem')) {
+      var settingString = html.window.localStorage['texas_holdem'];
+      print("111111111");
+      Map<String, dynamic> settingMap = json.decode(settingString);
+      var list = settingMap["players"] as List;
+      print("222222222" + json.encode(list));
+      List<Player> players = list.map((f) => PlayerStore(f["name"], f["gender"], f["count"], f["finalCash"], f["last"]).playerStore2Player()).toList();
+      print("333333333");
+      _players = players;
+      _ratio = settingMap["ratio"];
+    } else {
+      Map<String, dynamic> settingMap = {
+        "players": [],
+        "ratio": _ratio,
+      };
+      html.window.localStorage['texas_holdem'] = json.encode(settingMap);
+    }
+    _ratioController.text = _ratio;
   }
 
   void dispose() {
     _newPlayerNameController.dispose();
     super.dispose();
+  }
+
+  void _setPlayerLocalStorage(List<Player> pPlayers) {
+    if (html.window.localStorage.containsKey('texas_holdem')) {
+      var settingString = html.window.localStorage['texas_holdem'];
+      Map<String, dynamic> settingMap = json.decode(settingString);
+      var players = pPlayers.map((f) => {
+        "name": f.name,
+        "gender": f.gender,
+        "count": f.count,
+        "finalCash": f.finalCash,
+        "last": f.last.text,
+      }).toList();
+      settingMap["players"] = players;
+      html.window.localStorage['texas_holdem'] = json.encode(settingMap);
+    }
+  }
+
+  void _setRatioLocalStorage(String ratio) {
+    if (html.window.localStorage.containsKey('texas_holdem')) {
+      var settingString = html.window.localStorage['texas_holdem'];
+      Map<String, dynamic> settingMap = json.decode(settingString);
+      settingMap["ratio"] = ratio;
+      html.window.localStorage['texas_holdem'] = json.encode(settingMap);
+    }
   }
   
   void _dismissDialog() {
@@ -92,6 +208,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: new ListBody(
                   children: <Widget>[
                     new TextField(
+                      keyboardType: TextInputType.number,
                       autofocus: true,
                       controller: _newPlayerNameController,
                       decoration: new InputDecoration(
@@ -148,11 +265,12 @@ class _MyHomePageState extends State<MyHomePage> {
       Toast.show("Player's name existed", _scaffoldKey.currentContext, duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM, backgroundColor: Colors.redAccent);
       return;
     }
-    Player newPlayer = new Player(name, _gender, 1);
+    Player newPlayer = new Player(name, _gender, 1, 0);
     setState(() {
       _gender = 0;
       _players.add(newPlayer);
     });
+    _setPlayerLocalStorage(_players);
     print("Text field: $newPlayer");
     _dismissDialog();
     _newPlayerNameController.clear();
@@ -166,6 +284,7 @@ class _MyHomePageState extends State<MyHomePage> {
       setState(() {
         _players = _players;
       });
+      _setPlayerLocalStorage(_players);
     }
     _dismissDialog();
   }
@@ -201,6 +320,7 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _ratio = _ratioController.text;
     });
+    _setRatioLocalStorage(_ratioController.text);
     _dismissDialog();
   }
 
@@ -218,6 +338,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: new ListBody(
                   children: <Widget>[
                     new TextField(
+                      keyboardType: TextInputType.text,
                       autofocus: true,
                       controller: _ratioController,
                       decoration: new InputDecoration(
@@ -288,6 +409,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                       setState(() {
                                         _players = _players;
                                       });
+                                      _setPlayerLocalStorage(_players);
                                     }
                                   },
                                 ),
@@ -303,6 +425,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                       setState(() {
                                         _players = _players;
                                       });
+                                      _setPlayerLocalStorage(_players);
                                     }
                                   },
                                 ),
@@ -318,6 +441,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         Expanded(
                           flex: 1,
                           child: TextField(
+                            keyboardType: TextInputType.number,
                             decoration: new InputDecoration(
                               border: OutlineInputBorder(),
                               labelText: 'Last',
@@ -330,6 +454,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                 setState(() {
                                   _players = _players;
                                 });
+                                _setPlayerLocalStorage(_players);
                               }
                             },
                           ),
@@ -401,24 +526,25 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       );
     }
-    return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(
-        title: Text(widget.title),
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onPanDown: (_) {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: body,
+        floatingActionButton: CircleFloatingButton.floatingActionButton(
+          items: itemsActionBar,
+          color: Colors.deepPurple,
+          icon: Icons.settings,
+          duration: Duration(milliseconds: 500),
+          curveAnim: Curves.ease
+        ),
       ),
-      body: body,
-      floatingActionButton: CircleFloatingButton.floatingActionButton(
-        items: itemsActionBar,
-        color: Colors.deepPurple,
-        icon: Icons.settings,
-        duration: Duration(milliseconds: 500),
-        curveAnim: Curves.ease
-      ),
-      // FloatingActionButton(
-      //   onPressed: _addPlayer,
-      //   tooltip: '新增玩家',
-      //   child: Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
@@ -483,13 +609,6 @@ class SumaryPage extends StatelessWidget {
       body: body,
     );
   }
-}
-
-class SumaryArguments {
-  final List<Player> players;
-  final String ratio;
-
-  SumaryArguments(this.players, this.ratio);
 }
 
 class SlideLeftRoute extends PageRouteBuilder {
